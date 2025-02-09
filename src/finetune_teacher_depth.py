3d0f03d60f25cf8e530080e0af5a725ecb1b2ed3
PardisTaghavi <ptgh@tamu.edu>
2025-02-09 10:46:49 -0600

    fine tuning

--- src/finetune_teacher_depth.py
+++ src/finetune_teacher_depth.py
@@ -13,6 +13,8 @@ from cityscapesD import Cityscapes
 from loss import MidasLoss, MSGLoss, HDNLoss, scale_and_shift_mae
 from metrics import evaluate_depth
 import matplotlib.pyplot as plt
+from PIL import Image
+from torch.amp import autocast, GradScaler
 
 import sys
 path= "/home/avalocal/thesis23/KD/ml-depth-pro/src"
@@ -34,25 +36,48 @@ def parse_args():
     return parser.parse_args()
 
 def train(model, train_loader, criterion, optimizer, scheduler, device, epoch, args):
+    
     model.train()
     total_loss = 0
     grad_loss = MSGLoss()
-    scaler = torch.cuda.amp.GradScaler()
+    scaler = GradScaler()
+    H, W = 1536, 1536
+    f_px= 2262.52 #focal length in pixels
 
     for i, batch in enumerate(tqdm(train_loader, disable=(dist.get_rank() != 0))):
-        images, _, depths = batch  # Use actual depth labels
+        '''images, gt, depths = batch  # Use actual depth labels
         images, depths = images.to(device, non_blocking=True), depths.to(device, non_blocking=True)
         images = nn.functional.interpolate(images, size=(1536, 1536), mode='bilinear', align_corners=False)
-        
+        '''
+        images, _, gt_depth= batch #labels and depths are not used
+        images = images.to(device)
+        gt_depth = gt_depth.to(device)
+
+        valid_pixels = gt_depth > 0  # Mask for valid depth values (non-zero)
+        min_depth = torch.min(gt_depth[valid_pixels])  # Minimum valid depth
+        max_depth = torch.max(gt_depth[valid_pixels])  # Maximum valid depth
+        depth_normalized = torch.zeros_like(gt_depth)  # Initialize with zeros
+        depth_normalized[valid_pixels] = (gt_depth[valid_pixels] - min_depth) / (max_depth - min_depth)  # Normalize depth values [0-1]
+
         optimizer.zero_grad()
 
-        with torch.cuda.amp.autocast():
-            predictions = model(images)
-            predictions = predictions["depth"].squeeze(1)
-            loss_ssi = criterion(predictions, depths)
-            loss_grad = grad_loss(predictions, depths)
-            loss = 3.0 * loss_ssi + 2.0 * loss_grad
+        with autocast(device_type='cuda', dtype=torch.float16):
+            canonical_inverse_depth, fov_deg= model(images)
 
+            inverse_depth = canonical_inverse_depth * (W / f_px)
+            inverse_depth = nn.functional.interpolate(
+                inverse_depth, size=(H, W), mode="bilinear", align_corners=False
+            )
+            pred = 1.0 / torch.clamp(inverse_depth, min=1e-4, max=80)
+            pred = pred.squeeze(1) #B, 1, H, W -> B, H, W
+            pred = (pred - torch.min(pred)) / (torch.max(pred) - torch.min(pred))  # Normalize depth values [0-1]
+            
+            
+            loss_midas = criterion(pred, depth_normalized)
+            loss_grad = grad_loss(pred, depth_normalized)
+            loss = 3.0 * loss_midas + 2.0 * loss_grad
+        
+        print("------------?loss", loss)
         scaler.scale(loss).backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), 0.2)
         scaler.step(optimizer)
@@ -76,10 +101,26 @@ def validate(model, val_loader, criterion, device, epoch, args):
             images, _, gt_depth = batch
             images, gt_depth = images.to(device, non_blocking=True), gt_depth.to(device, non_blocking=True)
 
-            predictions = model.module.infer(images)  # Use model.module for DDP
-            predictions = predictions["depth"].squeeze(1)
-            loss_ssi = criterion(predictions, gt_depth)
-            loss_grad = grad_loss(predictions, gt_depth)
+            valid_pixels = gt_depth > 0  # Mask for valid depth values (non-zero)
+            min_depth = torch.min(gt_depth[valid_pixels])  # Minimum valid depth
+            max_depth = torch.max(gt_depth[valid_pixels])  # Maximum valid depth
+
+            # depth_normalized = np.zeros_like(gt_depth)  # Initialize with zeros
+            depth_normalized = torch.zeros_like(gt_depth)  # Initialize with zeros
+            depth_normalized[valid_pixels] = (gt_depth[valid_pixels] - min_depth) / (max_depth - min_depth)  # Normalize depth values
+            # print(f"Min depth: {min_depth}, Max depth: {max_depth}")
+
+            canonical_inverse_depth, fov_deg = model(images)
+            inverse_depth = canonical_inverse_depth * (1536 / 2262.52)
+            inverse_depth = nn.functional.interpolate(
+                inverse_depth, size=(1536, 1536), mode="bilinear", align_corners=False
+            )
+            predictions = 1.0 / torch.clamp(inverse_depth, min=1e-4, max=80)
+            predictions = predictions.squeeze(1)  # B, 1, H, W -> B, H, W
+            predictions = (predictions - torch.min(predictions)) / (torch.max(predictions) - torch.min(predictions))  # Normalize depth values [0-1]
+            
+            loss_ssi = criterion(predictions, depth_normalized)
+            loss_grad = grad_loss(predictions, depth_normalized)
             loss = 3.0 * loss_ssi + 2.0 * loss_grad
             total_loss += loss.item()
 
@@ -118,7 +159,6 @@ def main():
     model, _ = depth_pro.create_model_and_transforms()
     model = model.half().to(device)
     model = DDP(model, device_ids=[local_rank], output_device=local_rank, find_unused_parameters=True)
-
     optimizer = optim.AdamW(model.parameters(), lr=args.lr, weight_decay=0.001)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs * len(train_loader), eta_min=1e-8)
     criterion = MidasLoss()
